60e7239ee9444a5cf6e87d31f3c64e1845c0c608
Antonio Valentini <antonio.valentini@live.it>
2023-03-03 22:23:34 +0100

    Service scaffolding

---
+++ src/Gateway/Program.cs
@@ -0,0 +1,21 @@
+using GrpcServiceTests.Gateway.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddGrpcReflection();
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/",
+    () =>
+        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.MapGrpcReflectionService();
+
+app.Run();
